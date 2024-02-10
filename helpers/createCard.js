c15c6d75a013fe748639fe834a5711d534551d3f
VitaAtiv <142582763+VitaAtiv@users.noreply.github.com>
2024-02-10 15:11:47 +0300

    add

--- helpers/createCard.js
+++ helpers/createCard.js
@@ -12,6 +12,34 @@ export function createCard ({img, name, phone}) {
           </li>`;
 }
 
+import { refs } from "./refs.js";
+
+function createCard({ img, name, phone }) {
+refs.listEl.insertAdjacentHTML("afterbegin", "<li> img");
+
+  const liEl = document.createElement("li");
+  const imgEl = document.createElement("img");
+  const divEl = document.createElement("div");
+  const h3El = document.createElement("h3");
+  const pEl = document.createElement("p");
+  liEl.classList.add("list-item");
+  imgEl.classList.add("list-item-img");
+  imgEl.src = img;
+  imgEl.alt = name;
+  divEl.classList.add("list-item-wrap-text");
+  h3El.classList.add("list-item-title");
+  h3El.textContent = name;
+  pEl.classList.add("list-item-text");
+  pEl.textContent = phone;
+  divEl.append(h3El, pEl);
+  liEl.append(imgEl, divEl);
+  return liEl;
+}
+
+
+
+
+
 
 // function createCard({ img, name, phone }) {
 //   const liEl = document.createElement("li");
