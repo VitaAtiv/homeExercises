b35c5e84402c7fb401247963a1f260f5f28ef077
VitaAtiv <142582763+VitaAtiv@users.noreply.github.com>
2024-02-10 15:32:06 +0300

    add

--- helpers/createCard.js
+++ helpers/createCard.js
@@ -1,5 +1,23 @@
-export function createCard ({img, name, phone}) {
-     return `<li class="list-item">
+// export function createCard ({img, name, phone}) {
+//      return `<li class="list-item">
+//             <img
+//               class="list-item-img"
+//               src="${img}"
+//               alt=""
+//             />
+//             <div class="list-item-wrap-text">
+//               <h3 class="list-item-title">${name}</h3>
+//               <p class="list-item-text">${phone}</p>
+//             </div>
+//           </li>`;
+// }
+
+// import { refs } from "./refs.js";
+
+export function createCard({ img, name, phone }) {
+  refs.listEl.insertAdjacentHTML(
+    "afterbegin",
+    `<li class="list-item">
             <img
               class="list-item-img"
               src="${img}"
@@ -9,32 +27,27 @@ export function createCard ({img, name, phone}) {
               <h3 class="list-item-title">${name}</h3>
               <p class="list-item-text">${phone}</p>
             </div>
-          </li>`;
-}
+          </li>`
+  );
 
-import { refs } from "./refs.js";
-
-function createCard({ img, name, phone }) {
-refs.listEl.insertAdjacentHTML("afterbegin", "<li> img");
-
-  const liEl = document.createElement("li");
-  const imgEl = document.createElement("img");
-  const divEl = document.createElement("div");
-  const h3El = document.createElement("h3");
-  const pEl = document.createElement("p");
-  liEl.classList.add("list-item");
-  imgEl.classList.add("list-item-img");
-  imgEl.src = img;
-  imgEl.alt = name;
-  divEl.classList.add("list-item-wrap-text");
-  h3El.classList.add("list-item-title");
-  h3El.textContent = name;
-  pEl.classList.add("list-item-text");
-  pEl.textContent = phone;
-  divEl.append(h3El, pEl);
-  liEl.append(imgEl, divEl);
-  return liEl;
-}
+//   const liEl = document.createElement("li");
+//   const imgEl = document.createElement("img");
+//   const divEl = document.createElement("div");
+//   const h3El = document.createElement("h3");
+//   const pEl = document.createElement("p");
+//   liEl.classList.add("list-item");
+//   imgEl.classList.add("list-item-img");
+//   imgEl.src = img;
+//   imgEl.alt = name;
+//   divEl.classList.add("list-item-wrap-text");
+//   h3El.classList.add("list-item-title");
+//   h3El.textContent = name;
+//   pEl.classList.add("list-item-text");
+//   pEl.textContent = phone;
+//   divEl.append(h3El, pEl);
+//   liEl.append(imgEl, divEl);
+//   return liEl;
+// }
 
 
 
